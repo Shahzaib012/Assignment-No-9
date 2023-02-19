58ba32d8f91537b4f71b6e971a370ef9a3dca9b4
Shahzaib Nawaz <93723415+Shahzaib012@users.noreply.github.com>
2023-02-19 13:32:28 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,73 @@
+// Question no 1
+var myVar = 5;
+
+    console.log(myVar);
+
+// Question no 2
+var myString = "Hello World!";
+alert(myString);
+
+
+// Question no 3
+
+var num1 = 5;
+var num2 = 10;
+var num = prompt();
+let userNum = Number(num); 
+sum = num1+num2+userNum;
+console.log(sum);
+
+// Question no 4
+ var myBoolean = true;
+ if(myBoolean){
+    console.log("It is true");
+ }else{
+    console.log("false");
+ }
+
+// Question no 5
+var a = prompt();
+
+if(a%2==0){
+    console.log("it is even");
+}else if(a == 0){
+    console.log("It is even")
+}
+else {console.log("its odd");}
+
+// question no 6
+var a = "5";
+var b = "10";
+console.log("Result =",a +  b);
+
+// Queston No 7
+ var num1 = 8;
+ var num2 = 8;
+ result = num1*num2;
+ console.log(result);
+
+// Question No 8
+var a = prompt();
+if (a==="password"){
+    alert("Access Granted");
+}
+else{alert("Access Denied");}
+
+// Question No 9
+var a = prompt();
+var b = prompt();
+var c = prompt();
+if(a>b &&a>c){
+    console.log("Greater Number",a);
+}
+else if(b>a&&b>c){
+    console.log("Greater Number",b);
+}
+else{console.log("Greater Number",c);}
+
+// Question No 10
+var a = prompt();
+var b = prompt();
+if (a==b){
+    alert("The Strings are Equal");
+}else{alert("The strings are not Equal");}
